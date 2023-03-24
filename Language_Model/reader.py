60c3fe28ca59faf055d9249f709908c2baaa03b0
Kushagra Patidar <kushagra24ai027@satiengg.in>
2023-03-24 17:28:43 +0530

    POS & NER

--- Language_Model/reader.py
+++ Language_Model/reader.py
@@ -1,3 +1,5 @@
-file= open("rawCorpus.txt",'r')
-rawCorpus=file.read()
-print(f"Total no. of characters in read dataset: {len(rawCorpus)}")
+def readfile(fname):
+    corpusfile= open(fname,'r')
+    rawcorpus=corpusfile.read()
+    # print(f"Total no. of characters in read dataset: {len(rawCorpus)}")
+    return rawcorpus
